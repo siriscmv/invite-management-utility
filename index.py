7d7ea9b8d84c1c6aaacfa7a4cf35a97ac06b8652
Siris <vincentcmv@gmail.com>
2021-12-27 19:27:34 +0530

    rounding ping

--- index.py
+++ index.py
@@ -21,7 +21,7 @@ async def ping(msg: hikari.GuildMessageCreateEvent) -> None:
         return
 
     if msg.content.startswith(f"{config.get('prefix')}ping"):
-        await msg.message.respond(f"Pong! `{bot.heartbeat_latency}` ms")
+        await msg.message.respond(f"Pong! `{format(bot.heartbeat_latency * 1000, '.2f')}` ms")
 
 @bot.listen()
 async def verify(event: hikari.InteractionCreateEvent) -> None:
