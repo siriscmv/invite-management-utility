011f71c0a85bfe719b250e07005167ca93f4be88
Siris <vincentcmv@gmail.com>
2021-12-27 17:32:52 +0530

    simple verification

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
 import hikari
 import os
+
+from hikari.messages import MessageFlag
 from dotenv import load_dotenv
 import json
 
@@ -19,5 +22,19 @@ async def ping(msg: hikari.GuildMessageCreateEvent) -> None:
 
     if msg.content.startswith(f"{config.get('prefix')}ping"):
         await msg.message.respond("Pong!")
+
+@bot.listen()
+async def verify(interaction: hikari.ComponentInteraction) -> None:
+    if not interaction.guild_id == config.get('main_server'): return
+    if not interaction.custom_id == '_VERIFY': return
+
+    if interaction.user.created_at - datetime.now() < timedelta(days=7):
+        await interaction.create_initial_response(content="Your account is to younf to get verified, please DM a staff member to verify your account.", flags=MessageFlag.EPHEMERAL)
+        return
+    elif interaction.member.get_presence().visible_status.value == 'offline':
+        await interaction.create_initial_response(content="You cannot verify while being invisible!, please go online for a moment to verify and then go back to being invisible.", flags=MessageFlag.EPHEMERAL)
+    else:
+        await interaction.member.add_role(config.get('verified_role'))
+        await interaction.create_initial_response(content="You have been verified!", flags=MessageFlag.EPHEMERAL)
 
 bot.run( asyncio_debug=True, coroutine_tracking_depth=20, propagate_interrupts=True)
