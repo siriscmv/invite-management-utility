a2a66d025f0fb14692ae91def03f11f7da29aff0
Siris <vincentcmv@gmail.com>
2021-12-27 16:59:55 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,20 @@
+import hikari
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = hikari.GatewayBot(token = os.getenv('DISCORD_TOKEN'))
+
+@bot.listen()
+async def ping(msg: hikari.GuildMessageCreateEvent) -> None:
+    # If a non-bot user sends a message "hk.ping", respond with "Pong!"
+    # We check there is actually content first, if no message content exists,
+    # we would get `None' here.
+    if msg.is_bot or not msg.content:
+        return
+
+    if msg.content.startswith("-ping"):
+        await msg.message.respond("Pong!")
+
+bot.run( asyncio_debug=True, coroutine_tracking_depth=20, propagate_interrupts=True)
