824453f87d120879969653d7841801d5cc94f6c8
Siris <vincentcmv@gmail.com>
2021-12-27 17:01:41 +0530

    prefix

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 import hikari
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
+
+config = json.load('config.json')
 
 bot = hikari.GatewayBot(token = os.getenv('DISCORD_TOKEN'))
 
@@ -14,7 +17,7 @@ async def ping(msg: hikari.GuildMessageCreateEvent) -> None:
     if msg.is_bot or not msg.content:
         return
 
-    if msg.content.startswith("-ping"):
+    if msg.content.startswith(f"{config.prefix}ping"):
         await msg.message.respond("Pong!")
 
 bot.run( asyncio_debug=True, coroutine_tracking_depth=20, propagate_interrupts=True)
