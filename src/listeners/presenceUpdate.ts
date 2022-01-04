06caafe7edc4b6dfc13d1d1b1e4de84ba2668573
Siris <vincentcmv@gmail.com>
2022-01-04 21:13:24 +0530

    fix presenceUpdate

--- src/listeners/presenceUpdate.ts
+++ src/listeners/presenceUpdate.ts
@@ -5,11 +5,14 @@ import { logs, owners } from '../config.json';
 export class PresenceUpdateListener extends Listener {
 	public async run(oldPresence: Presence, newPresence: Presence) {
 		if (newPresence.userId !== '581451736305106985') return;
-		if (oldPresence && !newPresence)
+		if (oldPresence && (!newPresence || ['offline', 'invisible'].includes(newPresence.status)))
 			(oldPresence.client.channels.cache.get(logs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${oldPresence.user?.tag} has gone offline.`
 			});
-		else if (!oldPresence && newPresence)
+		else if (
+			(!oldPresence || ['offline', 'invisible'].includes(oldPresence.status)) &&
+			!['offline', 'invisible'].includes(newPresence.status)
+		)
 			(newPresence.client.channels.cache.get(logs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${newPresence.user?.tag} has come online.`
 			});
