dacef23e5b43022c9bc88e6b25a7c11b787ba2c1
Siris <vincentcmv@gmail.com>
2022-01-04 20:54:37 +0530

    test

--- src/listeners/presenceUpdate.ts
+++ src/listeners/presenceUpdate.ts
@@ -1,10 +1,10 @@
 import { Listener } from '@sapphire/framework';
 import type { Presence, TextChannel } from 'discord.js';
-import { mainBot, logs, owners } from '../config.json';
+import { logs, owners } from '../config.json';
 
 export class PresenceUpdateListener extends Listener {
 	public async run(oldPresence: Presence, newPresence: Presence) {
-		if (newPresence.user?.id !== mainBot) return;
+		if (newPresence.user?.id !== '581451736305106985') return;
 		if (oldPresence?.status && (!newPresence || newPresence.status === 'offline'))
 			(oldPresence.client.channels.cache.get(logs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${oldPresence.user?.tag} has gone offline.`
