2c70dc70fa99530e54730a45a48802a93b183e9b
Siris <vincentcmv@gmail.com>
2022-01-04 21:17:49 +0530

    log presence updates in sep channel

--- src/listeners/presenceUpdate.ts
+++ src/listeners/presenceUpdate.ts
@@ -1,19 +1,19 @@
 import { Listener } from '@sapphire/framework';
 import type { Presence, TextChannel } from 'discord.js';
-import { logs, owners, mainServer, mainBot } from '../config.json';
+import { uptimeLogs, owners, mainServer, mainBot } from '../config.json';
 
 export class PresenceUpdateListener extends Listener {
 	public async run(oldPresence: Presence, newPresence: Presence) {
 		if (newPresence.userId !== mainBot || newPresence.guild?.id !== mainServer) return;
 		if (oldPresence && (!newPresence || ['offline', 'invisible'].includes(newPresence.status)))
-			(oldPresence.client.channels.cache.get(logs)! as TextChannel).send({
+			(oldPresence.client.channels.cache.get(uptimeLogs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${oldPresence.user?.tag} has gone offline.`
 			});
 		else if (
 			(!oldPresence || ['offline', 'invisible'].includes(oldPresence.status)) &&
 			!['offline', 'invisible'].includes(newPresence.status)
 		)
-			(newPresence.client.channels.cache.get(logs)! as TextChannel).send({
+			(newPresence.client.channels.cache.get(uptimeLogs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${newPresence.user?.tag} has come online.`
 			});
 	}
