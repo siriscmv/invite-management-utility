f2e1528e20877b8488bc5a57b66bce3a06272ce9
Siris <vincentcmv@gmail.com>
2022-01-04 18:34:56 +0530

    new emotes

--- src/utils/emotes.ts
+++ src/utils/emotes.ts
@@ -1,5 +1,7 @@
 const emotes = {
-	timeout: '<:timeout:927886914055471134>'
+	timeout: '<:timeout:927886914055471134>',
+	yes: '<:YesTick:915291739063975966>',
+	no: '<:noCross:927910058287857755>'
 };
 
 export { emotes };
