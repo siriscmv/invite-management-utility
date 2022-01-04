36adb6c3c207cdd147dfb5a3d7c1e79bd2a9bc92
Siris <vincentcmv@gmail.com>
2022-01-04 21:03:55 +0530

    attempt to fix uncached user

--- src/listeners/presenceUpdate.ts
+++ src/listeners/presenceUpdate.ts
@@ -5,11 +5,11 @@ import { logs, owners } from '../config.json';
 export class PresenceUpdateListener extends Listener {
 	public async run(oldPresence: Presence, newPresence: Presence) {
 		if (newPresence.userId !== '581451736305106985') return;
-		if (oldPresence?.status && (!newPresence || newPresence.status === 'offline'))
+		if (oldPresence && !newPresence)
 			(oldPresence.client.channels.cache.get(logs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${oldPresence.user?.tag} has gone offline.`
 			});
-		else if (!oldPresence && newPresence.status)
+		else if (!oldPresence && newPresence)
 			(newPresence.client.channels.cache.get(logs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${newPresence.user?.tag} has come online.`
 			});
