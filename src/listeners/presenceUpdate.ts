9aa597aceec3e32cb07aefd590cab8760cdfd22b
Siris <vincentcmv@gmail.com>
2022-01-04 20:47:26 +0530

    rm returns

--- src/listeners/presenceUpdate.ts
+++ src/listeners/presenceUpdate.ts
@@ -6,11 +6,11 @@ export class PresenceUpdateListener extends Listener {
 	public async run(oldPresence: Presence, newPresence: Presence) {
 		if (newPresence.user?.id !== mainBot) return;
 		if (oldPresence.status && (!newPresence || newPresence.status === 'offline'))
-			return (oldPresence.client.channels.cache.get(logs)! as TextChannel).send({
+			(oldPresence.client.channels.cache.get(logs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${oldPresence.user?.tag} has gone offline.`
 			});
-		if (!oldPresence && newPresence.status)
-			return (newPresence.client.channels.cache.get(logs)! as TextChannel).send({
+		else if (!oldPresence && newPresence.status)
+			(newPresence.client.channels.cache.get(logs)! as TextChannel).send({
 				content: `${owners.map((id) => `<@${id}>`).join(', ')} | ${newPresence.user?.tag} has come online.`
 			});
 	}
