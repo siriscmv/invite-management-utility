e1e093e2cac55b964186f04aac45186d8415ed2f
Siris <vincentcmv@gmail.com>
2022-01-05 00:51:40 +0530

    fix cmd type

--- src/arguments/Command.ts
+++ src/arguments/Command.ts
@@ -1,12 +1,14 @@
-import { Argument, ArgumentContext, Command, PieceContext } from '@sapphire/framework';
-
+import { Argument, ArgumentContext, Command, CommandStore, PieceContext } from '@sapphire/framework';
+const { container } = require('@sapphire/framework');
 export class CommandArgument extends Argument<Command> {
 	public constructor(context: PieceContext) {
 		super(context, { name: 'commands', aliases: ['cmd'] });
 	}
 
 	public run(parameter: string, context: ArgumentContext<Command>): Argument.Result<Command> {
-		//const command = container.stores.get('commands');
+		const command: CommandStore = container.stores.get('commands');
+		if (command.has(parameter)) return this.ok(command.get(parameter)!);
+		if (command.some((c) => c.aliases.includes(parameter))) return this.ok(command.get(parameter)!);
 		return this.error({
 			parameter,
 			message: 'Unable to find a command',
