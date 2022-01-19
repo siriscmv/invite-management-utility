af54810da1d0af4e62d7740cbdeb41a752dc1e99
Siris <vincentcmv@gmail.com>
2022-01-19 14:25:53 +0530

    fix

--- src/commands/owner/eval.ts
+++ src/commands/owner/eval.ts
@@ -10,7 +10,6 @@ export class EvalCommand extends Command {
 		super(context, {
 			...options,
 			name: 'eval',
-			aliases: ['pong'],
 			description: 'Evals any JavaScript code',
 			quotes: [],
 			preconditions: ['OwnerOnly'],
