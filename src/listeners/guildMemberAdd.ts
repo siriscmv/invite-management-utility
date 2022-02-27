f00d90edff83d452eab45b7769701d321de5d712
Siris <vincentcmv@gmail.com>
2022-02-27 12:36:54 +0530

    revert: alts

--- src/listeners/guildMemberAdd.ts
+++ src/listeners/guildMemberAdd.ts
@@ -5,12 +5,12 @@ import { mainServer } from '../config.js';
 export class GuildMemberAddListener extends Listener {
 	public async run(member: GuildMember) {
 		if (member.guild.id !== mainServer) return;
-
+		/*
 		if (((member.client.db?.get('autoKickBypass') ?? []) as string[]).includes(member.id)) return;
 
-		const user = await member.user.fetch(true);
+		const user = await member.user.fetch();
 		if ((user.avatar && user.avatar.startsWith('a_')) || user.banner) return;
-
+		
 		if (Date.now() - user.createdTimestamp < user.client.db.get('altAge')!) {
 			await user
 				.send(
@@ -19,5 +19,6 @@ export class GuildMemberAddListener extends Listener {
 				.catch(() => {});
 			member.ban({ reason: 'Account too young' }).catch(() => {});
 		}
+		*/
 	}
 }
