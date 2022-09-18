921677e9362a93ee4a39edddbde6a174434e4957
Siris01 <vincentcmv@gmail.com>
2022-09-18 17:30:18 +0530

    feat: decancer & dehoist

--- src/listeners/guildMemberAdd.ts
+++ src/listeners/guildMemberAdd.ts
@@ -1,6 +1,10 @@
 import { Listener } from '@sapphire/framework';
 import type { GuildMember } from 'discord.js';
 import { mainBot, mainServer, verifiedRole } from '../config.js';
+import removeAccents from 'remove-accents'; //@ts-ignore
+import { nfkc } from 'unorm'; //@ts-ignore
+import latinize from 'latinize'; //@ts-ignore
+import unidecode from 'unidecode';
 
 export class GuildMemberAddListener extends Listener {
 	public async run(member: GuildMember) {
@@ -22,5 +26,29 @@ export class GuildMemberAddListener extends Listener {
 			member.ban({ reason: 'Account too young' }).catch(() => {});
 		}
 		*/
+
+		const deCancer = this.decancer(member.displayName);
+		if (deCancer) await member.setNickname(deCancer);
+		else return;
+
+		const deHoist = this.dehoist(member.displayName);
+		if (deHoist) await member.setNickname(deHoist);
+		else return;
+	}
+
+	private decancer(name: string) {
+		if (/^[\x00-\xFF]*$/im.test(name)) return null;
+		const sanitized: string = unidecode(nfkc(latinize(removeAccents(name)))).replace(/\[\?\]/g, '') || '';
+
+		if (sanitized.length > 2 && sanitized.length < 32) return sanitized;
+		return 'Moderated nickname';
+	}
+
+	private dehoist(name: string): string | null {
+		const isHoisted = (name: string) =>
+			name[0] < '0' || ['"', '(', "'", '<', '>', '=', '[', '>', '?'].includes(name[0]);
+
+		if (!isHoisted(name)) return null;
+		return name.length ? (isHoisted(name) ? this.dehoist(name.substring(1)) : name) : `Moderated nickname`;
 	}
 }
