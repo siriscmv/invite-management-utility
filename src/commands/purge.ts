ced6204c472aceb15d3a1e36f2d5dfa49ed15fff
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-02-22 20:22:28 +0530

    impl more commands

---
+++ src/commands/purge.ts
@@ -0,0 +1,26 @@
+import { Message, PermissionFlagsBits, TextChannel } from 'discord.js';
+import { Command } from '../utils/commands';
+
+const command: Command = {
+	name: 'purge',
+	shouldRun: (msg: Message) => {
+		return (msg.channel as TextChannel).permissionsFor(msg.member!)?.has(PermissionFlagsBits.ManageMessages);
+	},
+	run: async (msg: Message) => {
+		const amount = parseInt(msg.content.split(' ')[1]);
+		if (isNaN(amount)) return msg.reply('Invalid amount');
+
+		let count = 0;
+
+		while (count !== amount + 1) {
+			const remaining = amount - count;
+			const deleted = await (msg.channel as TextChannel).bulkDelete(Math.min(remaining, 100), true);
+			count += deleted.size;
+			if (deleted.size === 0) break;
+		}
+
+		return msg.reply(`Deleted ${count} messages`);
+	}
+};
+
+export default command;
--- src/commands/tags.ts
+++ src/commands/tags.ts
@@ -1,60 +1,57 @@
-import { Args, Command, SapphireClient } from '@sapphire/framework';
 import { Message } from 'discord.js';
+import { staffRoles } from 'src/config';
+import { Command } from '../utils/commands';
 
-export class TagsCommand extends Command {
-	public constructor(context: Command.Context, options: Command.Options) {
-		super(context, {
-			...options,
-			name: 'tags',
-			aliases: ['tag'],
-			description: 'Add/View/Delete/List a tag(s)',
-			quotes: [['"', '"']],
-			preconditions: ['StaffOnly'],
-			options: ['response', 'trigger']
-		});
+const command: Command = {
+	name: 'tags',
+	aliases: ['tag'],
+	shouldRun: (msg: Message) => {
+		if (msg.member!.roles.cache.some((r) => staffRoles.includes(r.id))) return true;
+		if (msg.content.split(' ')[1]) return 'Use the `tags` command to view all tags';
+		return false;
+	},
+	run: (msg: Message) => {
+		const subCommand = msg.content.split(' ')[1] ?? 'list';
+		switch (subCommand) {
+			case 'list':
+				return listTags(msg);
+			case 'add':
+				return addTag(msg);
+			case 'delete':
+				return deleteTag(msg);
+			default:
+				return msg.reply('Invalid subcommand');
+		}
 	}
+};
 
-	public async messageRun(message: Message, args: Args) {
-		const subCommand = await args.pick('string').catch(() => null);
-		if (subCommand === 'list') return this.list(message);
-		if (subCommand === 'view' || subCommand === 'show') return this.view(message, args);
-		if (subCommand === 'add' || subCommand === '+') return this.add(message, args);
-		if (subCommand === 'delete' || subCommand === '-') return this.delete(message, args);
-		return message.reply('Invalid subcommand.\nUse `list`, `view`, `add`, or `delete`.');
-	}
+export default command;
 
-	private async list(msg: Message) {
-		const tags = await (msg.client as SapphireClient).tags.raw.findAll();
-		const tagList = tags.map(
-			(tag: { trigger: any; createdAt: string | number | Date }) =>
-				`**${tag.trigger}** <t:${Math.round(new Date(tag.createdAt).getTime() / 1000)}:R>`
-		);
-		msg.channel.send(tagList.join('\n'));
-	}
+const listTags = async (msg: Message) => {
+	const tags = await (msg.client as SapphireClient).tags.raw.findAll();
+	const tagList = tags.map(
+		(tag: { trigger: any; createdAt: string | number | Date }) =>
+			`**${tag.trigger}** <t:${Math.round(new Date(tag.createdAt).getTime() / 1000)}:R>`
+	);
+	msg.channel.send(tagList.join('\n'));
+};
 
-	private async view(msg: Message, args: Args) {
-		const name = await args.rest('string');
-		const tag = msg.client.tags.get(name);
-		return msg.channel.send(tag ? JSON.parse(tag) : 'Tag not found.');
-	}
+const addTag = async (msg: Message) => {
+	const trigger = await args.pick('string');
+	const response = await args.rest('message').catch(() => args.rest('string'));
+	const data =
+		response instanceof Message
+			? JSON.stringify({ content: response.content, embeds: response.embeds, components: response.components })
+			: JSON.stringify(response);
 
-	private async add(msg: Message, args: Args) {
-		const trigger = await args.pick('string');
-		const response = await args.rest('message').catch(() => args.rest('string'));
-		const data =
-			response instanceof Message
-				? JSON.stringify({ content: response.content, embeds: response.embeds, components: response.components })
-				: JSON.stringify(response);
+	await msg.client.tags.set(trigger, data);
+	return msg.reply(`Tag added with name \`${trigger}\``);
+};
 
-		await msg.client.tags.set(trigger, data);
-		return msg.reply(`Tag added with name \`${trigger}\``);
-	}
-
-	private async delete(msg: Message, args: Args) {
-		const name = await args.rest('string');
-		const tag = msg.client.tags.get(name);
-		if (!tag) return msg.reply('Tag not found.');
-		await msg.client.tags.delete(name);
-		return msg.reply('Tag deleted.');
-	}
-}
+const deleteTag = async (msg: Message) => {
+	const name = await args.rest('string');
+	const tag = msg.client.tags.get(name);
+	if (!tag) return msg.reply('Tag not found.');
+	await msg.client.tags.delete(name);
+	return msg.reply('Tag deleted.');
+};
