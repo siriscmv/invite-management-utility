8d1c33cbdaffbad39b79346525415ed43bedbad6
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-02-22 21:57:40 +0530

    chore: fmt

--- src/events/messageDeleteBulk.ts
+++ src/events/messageDeleteBulk.ts
@@ -1,9 +1,8 @@
-
 import { EmbedBuilder, TextChannel, Message, Snowflake, GuildTextBasedChannel, Collection } from 'discord.js';
 import { mainServer, red, logChannel } from '../config.js';
 
 export default async function run(messages: Collection<Snowflake, Message>, channel: GuildTextBasedChannel) {
-	if ( channel.guildId !== mainServer) return;
+	if (channel.guildId !== mainServer) return;
 
 	const embed = new EmbedBuilder()
 		.setColor(red)
@@ -12,9 +11,7 @@ export default async function run(messages: Collection<Snowflake, Message>, chan
 		.setDescription(
 			`${messages.size} messages were deleted` //TODO: make this better
 		)
-		.addFields([
-			{ name: 'Channel', value: channel.toString(), inline: true },
-		]);
+		.addFields([{ name: 'Channel', value: channel.toString(), inline: true }]);
 
 	(channel.client.channels.cache.get(logChannel) as TextChannel)!.send({ embeds: [embed] });
 }
