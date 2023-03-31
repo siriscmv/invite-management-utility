7542f4323f7014e546938a4846198e0de48eb1e5
Siris01 <40269790+Siris01@users.noreply.github.com>
2023-03-31 18:53:31 +0530

    feat: ping status role

--- src/events/presenceUpdate.ts
+++ src/events/presenceUpdate.ts
@@ -5,17 +5,13 @@ export default async function run(oldPresence: Presence, newPresence: Presence) 
 	if (newPresence.userId !== mainBot || newPresence.guild?.id !== mainServer) return;
 	if (oldPresence && ['offline', 'invisible'].includes(newPresence.status))
 		(oldPresence.client.channels.cache.get(uptimeLogs)! as TextChannel).send({
-			content: `<:noCross:927910058287857755> | ${owners.map((id) => `<@${id}>`).join(', ')} | ${
-				oldPresence.user?.tag
-			} has gone offline.`
+			content: `<:noCross:927910058287857755> | <@&1091349273536385096> | ${oldPresence.user?.tag} has gone offline.`
 		});
 	else if (
 		(!oldPresence || ['offline', 'invisible'].includes(oldPresence.status)) &&
 		!['offline', 'invisible'].includes(newPresence.status)
 	)
 		(newPresence.client.channels.cache.get(uptimeLogs)! as TextChannel).send({
-			content: `<:YesTick:915291739063975966> | ${owners.map((id) => `<@${id}>`).join(', ')} | ${
-				newPresence.user?.tag
-			} has come online.`
+			content: `<:YesTick:915291739063975966> | <@&1091349273536385096> | ${newPresence.user?.tag} has come online.`
 		});
 }
